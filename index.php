fa854cc348676b6ea3b11480f2cd9999e7fb367e
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-09-06 13:21:30 +0200

    created a config file, login_register file, user file, admin file for php and added elements so I can be able to view elements added in the xampp for databases

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+
+session_start();
+
+$errors = [
+  'login' => $_SESSION['login_error'] ??'',
+  'login' => $_SESSION['register_error'] ??''
+];
+$activeForm = $_SESSION['active_form'] ?? 'login';
+
+session_unset();
+
+function showError($error) {
+  return !empty($error) ? "<p class='error-meesage'>$error</p>" : '';
+}
+
+function isActiveForm($formName, $activeForm) {
+  return $formNmae === $activeForm ? 'active' : '';
+}
+
+?>
+
 <html>
   <head>
     <title> login</title>
@@ -6,19 +28,22 @@
 
   <body>
     <div class="container">
-      <div class="form-box active" id="login-form"> 
-        <form action="">
+      <div class="form-box <?= isActiveForm('login', $activeForm); ?>" id="login-form"> 
+        <form action="login_register.php" method="post"> <!-- php file used to login --> 
           <h2>Login</h2>
+          <?= showError($errors['login']); ?>
             <input  type="idnumber" name="idnumber" placeholder="IDnumber" required>
             <input  type="password" name="password" placeholder="Password" required>
             <button type="submit" name="login">Login</button>
             <p class="small-text">Don’t have an account? <a href="#" onclick="showForm('register-form')">Register</a>  </p>
       </div>
 
-      <div class="form-box" id="register-form"> 
-        <form action="">
+      <div class="form-box <?= isActiveForm('register', $activeForm); ?>" id="register-form"> 
+        <form action="" method="post">
           <h2>Register</h2>
+          <?= showError($errors['register']); ?>
             <input  type="text" name="name" placeholder="Name" required>
+            <input  type="idnumber" name="idnumber" placeholder="IDnumber" required>
             <input  type="email" name="email" placeholder="Email" required>
             <input  type="password" name="password" placeholder="Password" required> 
             <select name="Type Of Stakeholder" required>
---
+++ login_register.php
@@ -0,0 +1,56 @@
+<?php
+
+session_start();
+require_once 'config.php';
+
+if (isset($_POST['register'])) {
+    $name = $_POST['name'];
+    $idnumber = $_POST['idnumber'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $role = $_POST['role'];
+
+    $checkIDnumber = $conn->query("SELECT idnumber FROM users WHERE idnumber = '$idnumber'")
+    if ($checkEmail->num_rows > 0){
+        $_SESSION['register_error'] = 'IDNUMBER is already registered!'
+        $_SESSION['active_form'] = 'register'
+    }else {
+        $conn->query("INSERT INTO users (name, idnumber, email, password, role) VALUES ('$name','$idnumber', '$email', '$password', '$role')");
+    }
+    $checkEmail = $conn->query("SELECT email FROM users WHERE email = '$email'")
+    if ($checkEmail->num_rows > 0){
+        $_SESSION['register_error'] = 'Email is already registered!'
+        $_SESSION['active_form'] = 'register'
+    }else {
+        $conn->query("INSERT INTO users (name, idnumber, email, password, role) VALUES ('$name','$idnumber', '$email', '$password', '$role')");
+    }
+    
+    header("Location: index.php");
+    exit();
+}
+
+if (isset($_POST['register'])) {
+    $idnumber = $_POST['idnumber'];
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM users WHERE idnumber = '$idnumber'")
+    if ($checkEmail->num_rows > 0){
+        $user = $result->Fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['email'] = $user['email'];
+            if ($user['role'] === 'admin') {
+                header("Location: admin_page.php");
+            }else {
+                header("Location: user_page.php")
+            }
+            exit();
+        }
+    }
+    $_SESSION['login_error'] = $user['incorrect idnumber or password'];
+    $_SESSION['active_form'] = 'login';
+    header("Location: index.php");
+    exit()
+}
+
+?>
