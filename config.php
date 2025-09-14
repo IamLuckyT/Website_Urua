7ceae40b458d53fff78da9a076e326f3f6177d72
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-09-14 04:50:38 +0200

    Still fixing bugs

--- config.php
+++ config.php
@@ -5,10 +5,10 @@ $user = "root";                                         //Gives access to the ro
 $password = "";                                         //Password is blank since its admin who has access
 $database = "users_db";                                 //Accesses the users database thats stored on the local host
 
-$conn = new mysql($host, $user,$password, $database);   //Creates a connection to the database
+$conn = new mysqli($host, $user,$password, $database);  //Creates a new object from the mysqli class
 
 if ($conn->connect_error){                              //Statement tracks if there are any errors found when connecting
-    die("connection failed: ". $conn->connect_error);   //if connection fails it kills the process
+    die("connection failed: ". $conn->connect_error);   //if connection fails it kills the processjj
 }
 
 ?>
