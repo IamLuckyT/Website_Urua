f45a395bb07c44466b7b73e3cf6ae9f29a87503e
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-09-14 03:31:32 +0200

    Added comments to allow easy understanding of the code

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
+//For allowing a connection between the database and the localhost
+$host = "localhost";                                    //For providing access to local host
+$user = "root";                                         //Gives access to the root file by allowing user to access root file
+$password = "";                                         //Password is blank since its admin who has access
+$database = "users_db";                                 //Accesses the users database thats stored on the local host
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "users_db";
+$conn = new mysql($host, $user,$password, $database);   //Creates a connection to the database
 
-$conn = new mysql($host, $user,$password, $database);
-
-if ($conn->connect_error){
-    die("connection failed: ". $conn->connect_error);
+if ($conn->connect_error){                              //Statement tracks if there are any errors found when connecting
+    die("connection failed: ". $conn->connect_error);   //if connection fails it kills the process
 }
 
 ?>
