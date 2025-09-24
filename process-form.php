0b08a438d8b2034600e2d77a4ecef2cb4347448b
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-09-24 10:53:16 +0200

    ...

--- process-form.php
+++ process-form.php
@@ -6,7 +6,7 @@ $type = filter_input(INPUT_POST, "type", FILTER_VALIDATE_INT);
 $terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOL);
 
 if (! $terms){
-    die("Terms must be accepted.")
+    die("Terms must be accepted.");
 } 
 
 $host = "localhost";
