e3fd3e8b7f97ea073e7e63cfae5abf10277958a9
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-09-24 10:35:48 +0200

    Linked the database to the process form file

--- process-form.php
+++ process-form.php
@@ -5,5 +5,23 @@ $priority = filter_input(INPUT_POST, "priority", FILTER_VALIDATE_INT);
 $type = filter_input(INPUT_POST, "type", FILTER_VALIDATE_INT);
 $terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOL);
 
-var_dump($name, $message, $priority, $type, $terms);        //For displaying data
+if (! $terms){
+    die("Terms must be accepted.")
+} 
+
+$host = "localhost";
+$dbname = "messages";
+$user = "root";
+$password = "";
+
+$conn = mysqli_connect(hostname: $host, 
+               username: $user,
+               password: $password,
+               database: $dbname);
+
+if (mysqli_connect_error()){
+    die("Connection error: " . mysqli_connect_error());
+}
+
+echo "Connection successful."
 ?>
